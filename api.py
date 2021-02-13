92b2942a56b7ffb8353b3f7fc44d35c38729f87c
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-02-13 15:09:47 +0530

    Change to production ready Cheroot WSGI server

--- api.py
+++ api.py
@@ -1,17 +1,36 @@
-from flask import Flask, jsonify, make_response, request
+# Cheroot for production ready WSGI server
+from cheroot.wsgi import Server as WSGIServer
+# Flask app requires
+from flask import Flask, jsonify, request
 from car_detection import predict as predict_cars
 import os
+# Werkzeug Utils for Securing file name
+from werkzeug.utils import secure_filename
+# Enable Logging before creating app
+import logging
+from logging.config import dictConfig
 
-app = Flask(__name__)
-
-
-# Root page
-@app.route('/')
-def root():
-    response = {
-        'message': 'This is Car Detection API'
+# Logging Config
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
     }
-    return jsonify(response), 200
+})
+
+# Make Flask App
+app = Flask(__name__)
+app.config['image_dir'] = 'images'
+app.config['allowed_files'] = ['jpg', 'jpeg']
 
 
 # Error Handler
@@ -20,8 +39,48 @@ def route_not_found(e):
     return jsonify({"message": "Invalid route"}), 404
 
 
+# Root page
+@app.route('/')
+def root():
+    response = {'message': 'This is Car Detection API'}
+    return jsonify(response), 200
+
+
+# Uplaod File
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Check if file is sent with request
+    if 'file' not in request.files:
+        return jsonify({'message': 'File not posted'}), 400
+
+    file = request.files['file']
+
+    # Check for empty file anme
+    if file.filename == '':
+        return jsonify({'message': 'File not posted'}), 400
+
+    # Check for file extension
+    if file.filename.rsplit('.', 1)[1].lower() not in app.config['allowed_files']:
+        return jsonify({'message': 'Invalid file extension'}), 400
+
+    # Secure the target file name
+    tgt_file = os.path.join(app.config['image_dir'],
+                            secure_filename(file.filename)
+                            )
+
+    # Write the file on server
+    file.save(tgt_file)
+
+    # Check for prediction flag in request
+    if os.path.isfile(tgt_file):
+        return jsonify({'message': 'File uploaded successfully',
+                        'filename': tgt_file}), 200
+    else:
+        return jsonify({"message": "Unable to upload file"}), 500
+
+
 # Prediction route
-@app.route('/predict')
+@app.route('/predict', methods=['GET'])
 def predict():
     img = request.args.get('img')
 
@@ -30,11 +89,21 @@ def predict():
     else:
         try:
             pred = predict_cars(img)
-            return jsonify({"message": "Successfully Predicted", "prediction": pred}), 200
+            return jsonify({"message": "Successfully Predicted",
+                            "prediction": pred}), 200
+
         except Exception as e:
             print(e)
             return jsonify({"message": "Image not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    #app.run(host='0.0.0.0', port='8080', debug=True)
+    server = WSGIServer(('0.0.0.0', 8080), app)
+    try:
+        logging.info('Starting the Server')
+        server.start()
+    except Exception as e:
+        logging.error('Server Stopped')
+        logging.error(e)
+        server.stop()
